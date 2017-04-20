7a5628b08dadc13f5ebc545ee1fbc59a29f929ec
Grady Shelton <gradyish@gmail.com>
2017-04-20 15:33:42 -0700

    add to method to exclude multiples up to 13. push to array with base primes up to 13. pass test

--- lib/prime_hunter.rb
+++ lib/prime_hunter.rb
@@ -1,5 +1,6 @@
 class Fixnum
   define_method(:prime_hunter) do
-    ((2..self).select{|n| n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0 && n%11!=0 && n%13!=0}).join","
+    array = [2,3,5,7,11,13]
+    array.push((2..self).select{|n| n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0 && n%11!=0 && n%13!=0}).join(", ")
   end
 end
--- spec/prime_hunter_spec.rb
+++ spec/prime_hunter_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('prime_hunter')
 
 describe('Fixnum#prime_number') do
-  it "diplays numbers up to input number, exluding multiples of 2" do
-    expect(10.prime_hunter).to(eq('3,5,7,9'))
+  # it "diplays numbers up to input number, exluding multiples of 2" do
+  #   expect(10.prime_hunter).to(eq('3,5,7,9'))
+  # end
+
+  it "diplays numbers up to input number, including 2, 3, 5, 7, 11, 13, but excluding their multiples" do
+    expect(100.prime_hunter).to(eq("2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97"))
   end
 end
