f220b4c38cd8630271c8e811e80063b6b3268f7a
Grady L Shelton <gradyish@gmail.com>
2017-06-16 08:05:41 -0700

    refactor prime hunter method

--- lib/prime_hunter.rb
+++ lib/prime_hunter.rb
@@ -1,9 +1,5 @@
 class Fixnum
   define_method(:prime_hunter) do
-    base_primes = [2,3,5,7,11,13]
-    array = []
-    until self == 14
-      array.push(base_primes)
-    array.push((2..self).select{|n| n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0 && n%11!=0 && n%13!=0}).join(", ")
+    return ((17..self).select{|n| n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0 && n%11!=0 && n%13!=0}).unshift(2,3,5,7,11,13).join(", ")
   end
 end
